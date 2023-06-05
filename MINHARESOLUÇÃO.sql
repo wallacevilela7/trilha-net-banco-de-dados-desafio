1ee612590fdd01059331c0b6bec1f0325842459a
Wallace Vilela <wallace.vilela9@gmail.com>
2023-06-05 11:52:26 -0300

    Descrição das queries

--- MINHARESOLUÇÃO.sql
+++ MINHARESOLUÇÃO.sql
@@ -1,32 +1,42 @@
 use Filmes;
  
+
+--1 - Buscar o nome e ano dos filmes
 SELECT Nome, Ano from Filmes;
 
+--2 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
 SELECT Nome, Ano from Filmes ORDER BY Ano;
 
+--3 - Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
 SELECT Nome, Ano, Duracao from Filmes where Nome = 'De volta para o Futuro';
 
+--4 - Buscar os filmes lançados em 1997
 SELECT Nome, Ano, Duracao from Filmes where Ano = 1997;
 
+--5 - Buscar os filmes lançados APÓS o ano 2000
 SELECT Nome, Ano, Duracao from Filmes where Ano > 2000;
 
+--6 - Buscar os filmes com a duracao maior que 100 e menor que 150, ordenando pela duracao em ordem crescente
 SELECT Nome, Ano, Duracao from Filmes where Duracao > 100 AND Duracao < 150
 ORDER BY Duracao;
 
+--7 - Buscar a quantidade de filmes lançadas no ano, agrupando por ano, ordenando pela duracao em ordem decrescente
 SELECT Ano, COUNT(*) AS QuantidadeFilmes FROM Filmes GROUP BY Ano ORDER BY MAX(Duracao) desc;
 
+--8 - Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
 SELECT PrimeiroNome, UltimoNome, Genero from Atores WHERE Genero = 'M';
 
+--9 - Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome
 SELECT PrimeiroNome, UltimoNome, Genero from Atores where Genero = 'F' ORDER BY PrimeiroNome asc;
 
-
+--10 - Buscar o nome do filme e o gênero
 SELECT Filmes.Nome, Generos.Genero AS Genero
 FROM Filmes
 JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
 JOIN Generos ON Generos.Id = FilmesGenero.IdGenero
 ORDER BY Filmes.Duracao DESC;
 
-
+--11 - Buscar o nome do filme e o gênero do tipo "Mistério"
 SELECT Filmes.Nome, Generos.Genero AS Genero
 FROM Filmes
 JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
@@ -34,6 +44,7 @@ JOIN Generos ON Generos.Id = FilmesGenero.IdGenero
 WHERE Generos.Genero = 'Mistério'
 ORDER BY Filmes.Duracao DESC;
 
+--12 - Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu Papel
 SELECT Filmes.Nome AS NomeFilme, Atores.PrimeiroNome, Atores.UltimoNome, ElencoFilme.Papel
 FROM Filmes
 JOIN ElencoFilme ON Filmes.Id = ElencoFilme.IdFilme
